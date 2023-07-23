dd44d3dae059efe95604d972212814ee32fa7385
Araghy <pooryaaraghy@ymail.com>
2023-07-23 17:30:32 +0300

    added gltf model

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { ARButton, XR, Controllers, Hands } from '@react-three/xr'
 import { Canvas } from '@react-three/fiber'
-
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 function App() {
+  const gltf = useLoader(GLTFLoader, 'https://cdn.jsdelivr.net/gh/siouxcitizen/3DModel@a1c2e47550ca20de421f6d779229f66efab07830/yuusha.gltf')
+
   return (
     <>
       <ARButton />
@@ -13,6 +16,7 @@ function App() {
           <Hands />
           <mesh>
             <boxGeometry />
+            <primitive object={gltf.scene} />
             <meshBasicMaterial color="blue" />
           </mesh>
         </XR>
